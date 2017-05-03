adb53df792f6884565e25f3a79b28672451818d2
Gian Franco Fioriello <GianFF@users.noreply.github.com>
2017-05-03 00:17:09 -0300

    Cierro el issue #17 - Mejorar el criterio de búsqueda de clientes: permitir nombre y apellido (#29)
    * Cierro el issue #17 - Mejorar el criterio de búsqueda de clientes: permitir nombre y apellido
    
    * Arreglo estilos en el resto de la app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :abogados, :controllers => {:registrations => 'abogados'}
   resources :clientes
-  get '/clientes/buscar/:nombre', controller: 'clientes', action: 'buscar'
 
   root 'clientes#new'
 end
