50ae622f643b749e1d9c6d07e2d480dc3634cddd
GianFF <gf.fioriello@gmail.com>
2017-06-25 19:17:18 -0300

    Refactorizo vista y código js del buscador

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -91,28 +91,35 @@ function comportamiento_buscador() {
 
     habilitar_enter_para_buscar();
 
+
+    function agregar_contenido_de_filas_al_elemnto(filas, elemento) {
+        if (filas.length === 0)
+            $(elemento).html('<h4>No se encontraron resultados</h4>').css('text-align', 'center');
+        else
+            $(elemento).html(filas).css('text-align', 'left');
+    }
+
     $('#panel_izquierdo__buscador').on('ajax:success', function (e, data, status, xhr) {
 
         var filas_clientes = data['clientes'].map(function( cliente ) {
             return "<tr><td>"+link_to(cliente['id'], cliente['nombre'], 'clientes')+"</td></tr>";
         });
-        $('#buscador_clientes__table_body').html(filas_clientes);
+        agregar_contenido_de_filas_al_elemnto(filas_clientes, '#buscador_clientes__table_body');
 
         var filas_expedientes = data['expedientes'].map(function( expediente ) {
             return "<tr><td>"+link_to(expediente['id'], expediente['titulo'], 'clientes/'+expediente['cliente_id']+'/expedientes')+"</td></tr>";
         });
-        $('#buscador_expedientes__table_body').html(filas_expedientes);
+        agregar_contenido_de_filas_al_elemnto(filas_expedientes, '#buscador_expedientes__table_body');
 
         var filas_escritos = data['escritos'].map(function( escrito ) {
             return "<tr><td>"+link_to(escrito['id'], escrito['titulo'], 'expedientes/'+escrito['expediente_id']+'/'+escrito['tipo'])+"</td></tr>";
         });
-        $('#buscador_escritos__table_body').html(filas_escritos);
+        agregar_contenido_de_filas_al_elemnto(filas_escritos, '#buscador_escritos__table_body');
 
         var filas_adjuntos = data['adjuntos'].map(function( adjunto ) {
             return "<tr><td>"+link_to(adjunto['id'], adjunto['titulo'], 'expedientes/'+adjunto['expediente_id']+'/adjuntos')+"</td></tr>";
         });
-        $('#buscador_adjuntos__table_body').html(filas_adjuntos);
-
+        agregar_contenido_de_filas_al_elemnto(filas_adjuntos, '#buscador_adjuntos__table_body');
 
         $('#buscador-modal').modal('show');
     });
